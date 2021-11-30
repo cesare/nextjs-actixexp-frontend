03c1ce36afa05b1fb3f21cd5e7bec68b0f276511
SAWADA Tadashi <cesare@mayverse.jp>
2021-11-30 21:37:40 +0900

    Refactor servant details with swr

--- pages/servants/[id].tsx
+++ pages/servants/[id].tsx
@@ -1,34 +1,26 @@
-import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import useSWR from 'swr'
 
 import Servant from '../../src/entities/Servant'
 import SingleServant from '../../src/backend/SingleServant'
-import { NotFound } from '../../src/backend/errors'
 
 export default function ShowServant() {
   const router = useRouter()
-  const [servant, setServant] = useState<Servant>()
-  const fetchServant = async () => {
-    if (router.isReady) {
-      const id = router.query.id as string
-      const value = await new SingleServant({ id: id }).execute()
-      setServant(value)
-    }
-  }
 
-  useEffect(() => {
-    fetchServant()
-  }, [router.query])
+  const fetcher = (_path: String): Promise<Servant> => {
+    const id = router.query.id as string
+    return new SingleServant({ id: id }).execute()
+  }
+  const { data, error } = useSWR(router.pathname, fetcher)
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
 
-  if (servant) {
-    return (
-      <div key={servant.id}>
-        <div>{servant.name}</div>
-        <div>[{servant.className}]</div>
-      </div>
-    )
-  }
-  else {
-    return <div />
-  }
+  const servant = data
+
+  return (
+    <div key={servant.id}>
+      <div>{servant.name}</div>
+      <div>[{servant.className}]</div>
+    </div>
+  )
 }
