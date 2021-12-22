0c2f03a221560765bf8634a3bbd9bb0a68a9cbbe
SAWADA Tadashi <cesare@mayverse.jp>
2021-12-22 20:45:31 +0900

    Fix table header

--- src/components/ServantList.tsx
+++ src/components/ServantList.tsx
@@ -22,7 +22,7 @@ class ServantList extends React.Component<Props> {
                       Name
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Title
+                      Class
                     </th>
                     <th scope="col" className="relative px-6 py-3">
                       <span className="sr-only">Edit</span>
